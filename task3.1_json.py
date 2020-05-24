b526a8cc49e0803c20ecd9c9d7dc02428c2d0984
kuzmin <k.v.kuzmin@yandex.ru>
2020-05-24 12:51:50 +0300

    Initial commit

---
+++ task3.1_json.py
@@ -0,0 +1,32 @@
+import json
+import collections
+
+def json_read():
+    with open('newsafr.json' , encoding='utf-8') as fi:
+        data = json.load(fi)
+        descript_list = []
+        for content in data['rss']['channel']['items']:
+            descript_list.append(content['description'].split())
+    return descript_list
+
+def sortByLength(inputStr):
+    return len(inputStr)
+
+def listmerge(json_read):
+    merged_list=[]
+    for lst in json_read:
+        merged_list.extend(lst)
+    top_words = []
+    for num , value in enumerate(merged_list):
+        if len(value) > 6:
+            top_words.append(merged_list[num])
+    top_words.sort(key=sortByLength , reverse=True)
+    return top_words
+
+def top_ten():
+    cnt = collections.Counter()
+    for word in listmerge(json_read()):
+        cnt[word] += 1
+    print(cnt.most_common(10))
+
+top_ten()
---
+++ task3.1_xml.py
@@ -0,0 +1,35 @@
+import xml.etree.ElementTree as ET
+import collections
+
+
+def xml_read():
+    parser = ET.XMLParser(encoding='utf-8')
+    tree = ET.parse('newsafr.xml', parser)
+    root = tree.getroot()
+    xml_items = root.findall('channel/item/description')
+    descript_list = []
+    for xmli in xml_items:
+        descript_list.append(xmli.text.split())
+    return descript_list
+
+def sortByLength(inputStr):
+    return len(inputStr)
+
+def listmerge(xml_read):
+    merged_list=[]
+    for lst in xml_read:
+        merged_list.extend(lst)
+    top_words = []
+    for num , value in enumerate(merged_list):
+        if len(value) > 6:
+            top_words.append(merged_list[num])
+    top_words.sort(key=sortByLength , reverse=True)
+    return top_words
+
+def top_ten():
+    cnt = collections.Counter()
+    for word in listmerge(xml_read()):
+        cnt[word] += 1
+    print(cnt.most_common(10))
+
+top_ten()
